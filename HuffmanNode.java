ec9c411c8fb7b4effb836d0aec3becf79770364e
llAkihitoll <saul.esteban.casti@gmail.com>
2025-04-30 06:16:49 -0600

    Add files via upload

---
+++ HuffmanNode.java
@@ -0,0 +1,32 @@
+/**
+ * Clase que representa un nodo en el árbol de Huffman.
+ */
+public class HuffmanNode implements Comparable<HuffmanNode> {
+    char character;
+    int frequency;
+    HuffmanNode left;
+    HuffmanNode right;
+
+    public HuffmanNode(char character, int frequency) {
+        this.character = character;
+        this.frequency = frequency;
+        this.left = null;
+        this.right = null;
+    }
+
+    public HuffmanNode(int frequency, HuffmanNode left, HuffmanNode right) {
+        this.character = '\0';
+        this.frequency = frequency;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int compareTo(HuffmanNode other) {
+        return Integer.compare(this.frequency, other.frequency);
+    }
+
+    public boolean isLeaf() {
+        return (this.left == null) && (this.right == null);
+    }
+}
